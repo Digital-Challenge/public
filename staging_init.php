15f074b99f240b325ddd8f7484ef8b3befb72143
pexlechris <pexlechris@gmail.com>
2025-09-19 12:09:52 +0300

    testing

--- staging_init.php
+++ staging_init.php
@@ -1,6 +1,12 @@
 <?php
-define('WP_DEBUG_DISPLAY', false);
+ini_set('display_errors', '1');
+ini_set('log_errors', '1');
+error_reporting(E_ERROR | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERROR | E_RECOVERABLE_ERROR);
 
-include __DIR__ . '/wp-load.php';
+
+define('WP_DEBUG', true);
+define('WP_DEBUG_LOG', true);
+
+require_once __DIR__ . '/wp-load.php';
 
 add_option('pextest1', 'ok');
