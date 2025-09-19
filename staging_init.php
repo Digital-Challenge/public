823303c02ea7687cbb6010a843b6e5e283639f08
pexlechris <pexlechris@gmail.com>
2025-09-19 12:12:46 +0300

    testing

--- staging_init.php
+++ staging_init.php
@@ -1,10 +1,8 @@
 <?php
-ini_set('display_errors', '1');
-ini_set('log_errors', '1');
-error_reporting(E_ERROR | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERROR | E_RECOVERABLE_ERROR);
-
+ini_set('display_errors', '0');
 
 define('WP_DEBUG', true);
+define('WP_DEBUG_DISPLAY', false);
 define('WP_DEBUG_LOG', true);
 
 require_once __DIR__ . '/wp-load.php';
