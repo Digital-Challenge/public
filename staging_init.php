b6a1a3262886db738d5bd2f292417f2963f43f0e
pexlechris <pexlechris@gmail.com>
2025-09-19 12:23:24 +0300

    testing

--- staging_init.php
+++ staging_init.php
@@ -1,8 +1,9 @@
 <?php
 // PHP-level suppression
 ini_set('display_errors', '0');
-ini_set('log_errors', '1');
-error_reporting(E_ERROR | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERROR | E_RECOVERABLE_ERROR);
+ini_set('log_errors', '0');
+error_reporting(0);
+
 
 // WordPress debug constants
 define('WP_DEBUG', false);
