bafe19c308624f21057ab4c945dd0c3f547c9411
pexlechris <pexlechris@gmail.com>
2025-09-19 15:14:30 +0300

    dc_staging_enable_coming_soon() ready

--- staging_init.php
+++ staging_init.php
@@ -12,8 +12,47 @@ define('WP_DEBUG_LOG', true);
 
 require_once __DIR__ . '/wp-load.php';
 
+$functions = [
+	'dc_staging_enable_coming_soon',
+];
+
+
 // Your logic
-add_option('pextest1', 'ok');
-echo '-> WP: Option added';
 echo "\n";
+
+foreach ($functions as $function) {
+	ob_start();
+
+	try {
+		call_user_func( $function );
+	} catch ( \Throwable $e ) {
+		echo "Exception in {$function}: {$e->getMessage()}\nTrace:\n{$e->getTraceAsString()}";
+	}
+
+	$echo = ob_get_clean();
+	echo "-> WP: $echo";
+	echo "\n";
+	echo "\n";
+}
+
+function dc_staging_enable_coming_soon(){
+
+	// Fully qualified class name
+	$fqcn = '\Automattic\WooCommerce\Admin\API\LaunchYourStore';
+	$method = 'initialize_coming_soon';
+
+	if ( !class_exists( $fqcn ) || !method_exists( $fqcn , $method ) ) {
+		echo "Coming Soon not enabled. Use plugin or htaccess for this scope";
+		return;
+	}
+
+	$return1 = update_option( 'woocommerce_coming_soon', 'yes' );
+	$return2 = update_option( 'woocommerce_store_pages_only', 'no' );
+
+	if ( $return1 && $return2 ) {
+		echo "Coming Soon initialized successfully";
+	} else {
+		echo "Coming Soon not enabled. Use plugin or htaccess for this scope";
+	}
+}
 
